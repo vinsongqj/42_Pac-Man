b461e86f2c57c7e54736c9e0341e75b15e54490d
AlexHysel <alexhysel@gmail.com>
2026-09-14 16:21:14 +0800

    Add Unique names rule

--- src/server/Pacman.Server.Data/UserDb.cs
+++ src/server/Pacman.Server.Data/UserDb.cs
@@ -5,4 +5,13 @@ namespace Pacman.Server.Data;
 class PacmanDb(DbContextOptions<PacmanDb> options) : DbContext(options)
 {
     public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(p => p.Name)
+            .IsUnique();
+    }
 }
