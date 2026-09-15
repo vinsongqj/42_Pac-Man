d94fce5fc5320529418bb6e9fdcdf1e13d4b14fc
AlexHysel <alexhysel@gmail.com>
2026-09-15 20:34:35 +0800

    refactor

---
+++ src/server/Pacman.Server.Core/Contracts/Requests.cs
@@ -0,0 +1,10 @@
+public record LoginRequest(string name, string password)
+{
+    public string Name = name;
+    public string Password = password;
+}
+
+public record UpdateBestScoreRequest(int newScore)
+{
+    public int NewScore = newScore;
+}
