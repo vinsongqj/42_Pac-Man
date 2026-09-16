86db9d53ba83958d3859da36643019021ed5eff7
AlexHysel <alexhysel@gmail.com>
2026-09-16 13:16:16 +0800

    add GetScoreboard method

--- src/server/Pacman.Server.Core/Contracts/Responses.cs
+++ src/server/Pacman.Server.Core/Contracts/Responses.cs
@@ -6,7 +6,7 @@ public record UserDTO(string id, string name, int bestScore)
     string Name = name;
     int BestScore = bestScore;
 
-    public UserDTO FromUser(User user)
+    public static UserDTO FromUser(User user)
     {
         return new UserDTO(user.Id.ToString(), user.Name, user.BestScore);
     }
--- src/server/Pacman.Server.Core/Orchestrator.cs
+++ src/server/Pacman.Server.Core/Orchestrator.cs
@@ -1,4 +1,5 @@
-﻿using Pacman.Server.Data;
+﻿using System.Runtime.CompilerServices;
+using Pacman.Server.Data;
 
 namespace Pacman.Server.Core;
 
@@ -9,5 +10,11 @@ public class Orchestrator
     public Orchestrator(IDbManager manager)
     {
         _manager = manager;
+    }
+
+    public async Task<List<UserDTO>> GetScoreboardAsync(int size)
+    {
+        List<User> users = await _manager.GetUsersByScoreAsync(size);
+        return users.Select(u => UserDTO.FromUser(u)).ToList();
     }
 }
