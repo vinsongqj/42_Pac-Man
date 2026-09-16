a038323e51a8923ba7e09c8c25a231ef0907e8db
AlexHysel <alexhysel@gmail.com>
2026-09-16 13:34:27 +0800

    add Signup and Login methods

--- src/server/Pacman.Server.Core/Orchestrator.cs
+++ src/server/Pacman.Server.Core/Orchestrator.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Pacman.Server.Data;
+﻿using Pacman.Server.Data;
 
 namespace Pacman.Server.Core;
 
@@ -16,5 +15,22 @@ public class Orchestrator
     {
         List<User> users = await _manager.GetUsersByScoreAsync(size);
         return users.Select(u => UserDTO.FromUser(u)).ToList();
+    }
+
+    public async Task<bool> Signup(LoginRequest request)
+    {
+        if (request.name.Length < 3) return false;
+        if (request.password.Length < 4) return false;
+        if (await _manager.UserExistsAsync(request.name)) return false;
+        await _manager.CreateUserAsync(request.name, request.password);
+        return true;
+    }
+
+    public async Task<bool> Login(LoginRequest request)
+    {
+        User? user = await _manager.GetUserAsync(request.name);
+        if (user == null) return false;
+        if (user.Password != request.Password) return true;
+        return true;
     }
 }
